11d282ad2e02f64f4385409a53a5d87b8e1902b6
Federico Daniel Colombo <thepirat000@hotmail.com>
2015-09-15 22:39:53 -0500

    updating nuspec

--- Properties/AssemblyInfo.cs
+++ Properties/AssemblyInfo.cs
@@ -6,7 +6,7 @@ using System.Runtime.InteropServices;
 // set of attributes. Change these attribute values to modify the information
 // associated with an assembly.
 [assembly: AssemblyTitle("Automatic Factory")]
-[assembly: AssemblyDescription("A Generic Factory for strategies using DI containers")]
+[assembly: AssemblyDescription("A factory creator for strategies using AutoFac. Never write a Factory again!.")]
 [assembly: AssemblyConfiguration("")]
 [assembly: AssemblyCompany("Federico Colombo")]
 [assembly: AssemblyProduct("AutoFactory")]
@@ -32,5 +32,5 @@ using System.Runtime.InteropServices;
 // You can specify all the values or you can default the Build and Revision Numbers 
 // by using the '*' as shown below:
 // [assembly: AssemblyVersion("1.0.*")]
-[assembly: AssemblyVersion("2.0.0.1")]
-[assembly: AssemblyFileVersion("2.0.0.1")]
+[assembly: AssemblyVersion("2.0.0.3")]
+[assembly: AssemblyFileVersion("2.0.0.3")]
